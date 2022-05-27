80f45a367e3dc0fa285d9e085f056306923746ff
«viktoriya <viki.lar@yandex.ru>
2022-05-27 12:18:35 +0400

    ввод переменных и вывод пееменных в консоль

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = "First Project";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 25;
+let rollback = 78;
+let fullPrice = 130;
+let adaptive = true;
 
 console.log("Привет, Мир!");
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
 alert("Hello world!");
+
+/*Вывести в консоль тип данных значений переменных title, fullPrice, adaptive;
+Вывести в консоль длину строки из переменной screens
+Вывести в консоль “Стоимость верстки экранов (screenPrice) рублей/ долларов/гривен/юани” и “Стоимость разработки сайта (fullPrice) рублей/ долларов/гривен/юани”
+Привести строку screens к нижнему регистру и разбить строку на массив, вывести массив в консоль
+Вывести в консоль Процент отката посреднику за работу (fullPrice * (rollback/100))*/
