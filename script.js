6c860b859ec007e3635188b3cd695d771ad8ffee
«viktoriya <viki.lar@yandex.ru>
2022-05-30 18:59:33 +0400

    добавлены условия в оператор switch

--- script.js
+++ script.js
@@ -2,16 +2,34 @@
 
 let title = prompt("Как называется Ваш проект?");
 let screens = prompt("Какие типы экранов нужно разработать?");
-let screenPrice = Number(prompt("Сколько будет стоить данная работа?"));
+let screenPrice = parseInt(prompt("Сколько будет стоить данная работа?"));
 let adaptive = Boolean(confirm("Нужен ли адаптив на сайте?"));
 let servise01 = prompt("Какой дополнительный тип услуги нужен?");
-let servisePrice1 = Number(prompt("Сколько это будет стоить?"));
+let servisePrice1 = parseInt(prompt("Сколько это будет стоить?"));
 let servise02 = prompt("Какой дополнительный тип услуги нужен?");
-let servisePrice2 = Number(prompt("Сколько это будет стоить?"));
+let servisePrice2 = parseInt(prompt("Сколько это будет стоить?"));
 let fullPrice = screenPrice + servisePrice1 + servisePrice2;
 let rollback = 48;
 let percent = fullPrice * (rollback / 100);
 let servicePercentPrice = Math.ceil(fullPrice - percent);
+console.log(servicePercentPrice);
+
+switch (true) {
+  case fullPrice >= 30000:
+    console.log("Даем скидку в 10%");
+    break;
+  case fullPrice >= 15000:
+  case fullPrice < 30000:
+    console.log("Даем скидку в 5%");
+    break;
+  case fullPrice < 15000:
+  case fullPrice >= 0:
+    console.log("Скидка не предусмотрена");
+    break;
+  default:
+    console.log("Что-то пошло не так");
+    break;
+}
 
 console.log("Привет, Мир!");
 console.log(typeof title);
