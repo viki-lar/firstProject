87d55621623ce548d8e09eeb46b9e398cdf19a8c
«viktoriya <viki.lar@yandex.ru>
2022-05-30 12:33:33 +0400

    ввод переменных и рассчет стоимости

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-let title = "First Project";
-let screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 25;
+"use strict";
+
+let title = prompt("Как называется Ваш проект?");
+let screens = prompt("Какие типы экранов нужно разработать?");
+let screenPrice = Number(prompt("Сколько будет стоить данная работа?"));
+let adaptive = Boolean(confirm("Нужен ли адаптив на сайте?"));
+let servise01 = prompt("Какой дополнительный тип услуги нужен?");
+let servisePrice1 = Number(prompt("Сколько это будет стоить?"));
+let servise02 = prompt("Какой дополнительный тип услуги нужен?");
+let servisePrice2 = Number(prompt("Сколько это будет стоить?"));
+let fullPrice = screenPrice + servisePrice1 + servisePrice2;
 let rollback = 48;
-let fullPrice = 130;
-let adaptive = true;
 let percent = fullPrice * (rollback / 100);
+let servicePercentPrice = Math.ceil(fullPrice - percent);
 
 console.log("Привет, Мир!");
 console.log(typeof title);
-console.log(typeof fullPrice);
+console.log(fullPrice);
 console.log(typeof adaptive);
 console.log(screens.length);
 console.log(
