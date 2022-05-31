0a24cf57498a46ded0b4cefe45749158dd4adf2e
«viktoriya <viki.lar@yandex.ru>
2022-05-31 22:07:44 +0400

    выполнены расчеты с использованием функций

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+//объявление переменных
+
 let title = prompt("Как называется Ваш проект?");
 let screens = prompt("Какие типы экранов нужно разработать?");
 let screenPrice = parseInt(prompt("Сколько будет стоить данная работа?"));
@@ -12,37 +14,55 @@ let fullPrice = screenPrice + servisePrice1 + servisePrice2;
 let rollback = 48;
 let percent = fullPrice * (rollback / 100);
 let servicePercentPrice = Math.ceil(fullPrice - percent);
-console.log(servicePercentPrice);
+let allServicePrices;
 
-switch (true) {
-  case fullPrice >= 30000:
-    console.log("Даем скидку в 10%");
-    break;
-  case fullPrice >= 15000:
-  case fullPrice < 30000:
-    console.log("Даем скидку в 5%");
-    break;
-  case fullPrice < 15000:
-  case fullPrice >= 0:
-    console.log("Скидка не предусмотрена");
-    break;
-  default:
-    console.log("Что-то пошло не так");
-    break;
+//объявление функций
+
+const showTypeOf = function (variable) {
+  console.log(variable, typeof variable);
+};
+
+const getRollbackMessage = function (price) {
+  switch (true) {
+    case price >= 30000:
+      return "Даем скидку в 10%";
+    case price >= 15000:
+    case price < 30000:
+      return "Даем скидку в 5%";
+    case price < 15000:
+    case price >= 0:
+      return "Скидка не предусмотрена";
+    default:
+      return "Что-то пошло не так";
+  }
+};
+
+const getAllServicePrices = function (priceServise01, priceServise02) {
+  return priceServise01 + priceServise02;
+};
+
+function getFullPrice() {
+  fullPrice = screenPrice + allServicePrices;
 }
 
-console.log("Привет, Мир!");
-console.log(typeof title);
-console.log(fullPrice);
-console.log(typeof adaptive);
-console.log(screens.length);
-console.log(
-  "Стоимость верстки экранов",
-  screenPrice,
-  "долларов \nСтоимость разработки сайта",
-  fullPrice,
-  " долларов"
-);
-screens = screens.toLowerCase();
-console.log(screens.split(", "));
-console.log(percent);
+const getTitle = function (nameTitle) {
+  nameTitle = nameTitle.replace(/^\s/, "");
+  nameTitle = nameTitle.toLowerCase();
+  return nameTitle.charAt(0).toUpperCase() + nameTitle.substring(1);
+};
+
+const getServicePercentPrices = function (totalPrice, percentPrice) {
+  return totalPrice - percentPrice;
+};
+
+// блок функционала
+showTypeOf(title);
+showTypeOf(screenPrice);
+showTypeOf(adaptive);
+servicePercentPrice = getServicePercentPrices(fullPrice, percent);
+allServicePrices = getAllServicePrices(servisePrice1, servisePrice2);
+
+// вывод в консоль
+console.log(screens);
+console.log(getRollbackMessage(fullPrice));
+console.log(servicePercentPrice);
