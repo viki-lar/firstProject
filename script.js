a58c3e954cc89f13ac38fc7506376a524349d825
«viktoriya <viki.lar@yandex.ru>
2022-06-09 22:22:59 +0400

    вывод времени и даты в формате часов

--- script.js
+++ script.js
@@ -2,67 +2,125 @@
 
 //объявление переменных
 
-let title = prompt("Как называется Ваш проект?");
-let screens = prompt("Какие типы экранов нужно разработать?");
-let screenPrice = parseInt(prompt("Сколько будет стоить данная работа?"));
-let adaptive = Boolean(confirm("Нужен ли адаптив на сайте?"));
-let servise01 = prompt("Какой дополнительный тип услуги нужен?");
-let servisePrice1 = parseInt(prompt("Сколько это будет стоить?"));
-let servise02 = prompt("Какой дополнительный тип услуги нужен?");
-let servisePrice2 = parseInt(prompt("Сколько это будет стоить?"));
-let fullPrice = screenPrice + servisePrice1 + servisePrice2;
-let rollback = 48;
-let percent = fullPrice * (rollback / 100);
-let servicePercentPrice = Math.ceil(fullPrice - percent);
-let allServicePrices;
+const clock = function () {
+  const timeOne = document.querySelector(".today");
+  const now = document.querySelector(".time");
+  const today = new Date();
+  const day = today.getDate();
+  let dayWeek = today.getDay() - 1;
+  let month = today.getMonth();
+  const year = today.getFullYear();
+  const hours = today.getHours();
+  const minutes = today.getMinutes();
+  const seconds = today.getSeconds();
+  let nameHours;
+  let nameMinutes;
+  let nameSeconds;
+  const week = [
+    "Понедельник",
+    "Вторник",
+    "Среда",
+    "Четверг",
+    "Пятница",
+    "Суббота",
+    "Воскресенье",
+  ];
 
-//объявление функций
+  const nameMonth = [
+    "Января",
+    "Февраля",
+    "Марта",
+    "Апреля",
+    "Мая",
+    "Июня",
+    "Июля",
+    "Августа",
+    "Сентября",
+    "Октября",
+    "Ноября",
+    "Декабря",
+  ];
 
-const showTypeOf = function (variable) {
-  console.log(variable, typeof variable);
-};
-
-const getRollbackMessage = function (price) {
-  switch (true) {
-    case price >= 30000:
-      return "Даем скидку в 10%";
-    case price >= 15000:
-    case price < 30000:
-      return "Даем скидку в 5%";
-    case price < 15000:
-    case price >= 0:
-      return "Скидка не предусмотрена";
-    default:
-      return "Что-то пошло не так";
+  for (let key in week) {
+    if (dayWeek == key) {
+      dayWeek = week[key];
+    }
   }
+
+  for (let key in nameMonth) {
+    if (month == key) {
+      month = nameMonth[key];
+    }
+  }
+
+  if (hours % 10 == 1 && hours != 11) {
+    nameHours = "час";
+  } else if (hours % 10 <= 4 && hours != 0) {
+    nameHours = "часа";
+  } else {
+    nameHours = "часов";
+  }
+
+  if (minutes % 10 == 1 && minutes != 11) {
+    nameMinutes = "минута";
+  } else if (
+    minutes % 10 <= 4 &&
+    minutes != 11 &&
+    minutes != 12 &&
+    minutes != 13 &&
+    minutes != 14 &&
+    minutes % 10 != 0
+  ) {
+    nameMinutes = "минуты";
+  } else {
+    nameMinutes = "минут";
+  }
+
+  if (seconds % 10 == 1 && seconds != 11) {
+    nameSeconds = "секунда";
+  } else if (
+    seconds % 10 <= 4 &&
+    seconds % 10 != 0 &&
+    seconds != 12 &&
+    seconds != 13 &&
+    seconds != 14
+  ) {
+    nameSeconds = "секунды";
+  } else {
+    nameSeconds = "секунд";
+  }
+
+  timeOne.textContent =
+    "Сегодня" +
+    " " +
+    dayWeek +
+    ", " +
+    day +
+    " " +
+    month +
+    " " +
+    year +
+    " " +
+    "года" +
+    ", " +
+    hours +
+    " " +
+    nameHours +
+    " " +
+    minutes +
+    " " +
+    nameMinutes +
+    " " +
+    seconds +
+    " " +
+    nameSeconds;
+
+  //
+  const time = function () {
+    now.textContent =
+      today.toLocaleDateString() + "-" + today.toLocaleTimeString();
+  };
+  time();
 };
-
-const getAllServicePrices = function (priceServise01, priceServise02) {
-  return priceServise01 + priceServise02;
-};
-
-function getFullPrice() {
-  fullPrice = screenPrice + allServicePrices;
-}
-
-const getTitle = function (nameTitle) {
-  nameTitle = nameTitle.replace(/^\s/, "");
-  nameTitle = nameTitle.toLowerCase();
-  return nameTitle.charAt(0).toUpperCase() + nameTitle.substring(1);
-};
-
-const getServicePercentPrices = function (totalPrice, percentPrice) {
-  return totalPrice - percentPrice;
-};
-
-// блок функционала
-showTypeOf(title);
-showTypeOf(screenPrice);
-showTypeOf(adaptive);
-servicePercentPrice = getServicePercentPrices(fullPrice, percent);
-allServicePrices = getAllServicePrices(servisePrice1, servisePrice2);
-
-// вывод в консоль
-console.log(screens);
-console.log(getRollbackMessage(fullPrice));
-console.log(servicePercentPrice);
+setInterval(clock, 1000);
+clock();
